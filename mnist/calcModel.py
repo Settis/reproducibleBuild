0f05c63a03218f416e3afca9b75888c1c678e0d6
Anton <strannik.anton@gmail.com>
2022-08-09 01:18:27 +0200

    Add another seeds into mnist

--- mnist/calcModel.py
+++ mnist/calcModel.py
@@ -18,9 +18,9 @@ ops.reset_default_graph()
 
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10)
+  tf.keras.layers.Dense(128, activation='relu', kernel_initializer=tf.random_uniform_initializer(seed=int(config['Model']['seed']))),
+  tf.keras.layers.Dropout(0.2, seed=int(config['Model']['seed'])),
+  tf.keras.layers.Dense(10, kernel_initializer=tf.random_uniform_initializer(seed=int(config['Model']['seed'])))
 ])
 
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
@@ -29,7 +29,7 @@ model.compile(optimizer='adam',
               loss=loss_fn,
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=5)
+model.fit(x_train, y_train, epochs=5, verbose=2)
 
 model.evaluate(x_test,  y_test, verbose=2)
 
